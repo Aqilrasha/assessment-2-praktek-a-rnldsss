3662032e91a01b119c02618e91f17922bfaa4edd
Aqilrasha <aqilarasha2@gmail.com>
2024-12-12 08:26:26 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -5,11 +5,23 @@ public class Nomor1 {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
+          public static void tampilkanGanjil(int N) {
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
 
-        /* Pekerjaan anda berakhir sini */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    
+        System.out.print("Masukkan bilangan N: ");
+        int N = scanner.nextInt();
+        
+        tampilkanGanjil(n);
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
     }
-    
+
 }
